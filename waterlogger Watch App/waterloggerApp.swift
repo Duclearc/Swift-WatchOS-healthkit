d445d0eb89ba2caa04914b66551880407b4a9665
Dan <59476105+Duclearc@users.noreply.github.com>
2025-07-19 13:18:28 +0200

    Initial Commit

---
+++ waterlogger Watch App/waterloggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterloggerApp.swift
+//  waterlogger Watch App
+//
+//  Created by Dan Pina on 19.07.25.
+//
+
+import SwiftUI
+
+@main
+struct waterlogger_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
